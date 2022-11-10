5ab990c5d044219ccccd1f7f1fb21d731b0c1575
Tom Lynch <tomlynch@TL-DivideBuyMBPro.local>
2022-11-10 22:05:30 +0000

    Add GeneratesUuidOnCreation concern

---
+++ src/Concerns/GeneratesUuidOnCreation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Concerns;
+
+use Illuminate\Support\Str;
+
+trait GeneratesUuidOnCreation
+{
+    public static function bootGeneratesUuidOnCreation()
+    {
+        static::creating(function ($model) {
+            // Ensure new models have a UUID when created
+            if (empty($model->uuid)) {
+                $model->uuid = Str::uuid();
+            }
+        });
+    }
+}
